ebc3cea7e84416493f8f54e94364ce4fdcefa078
Olivia Chen <baomien@gmail.com>
2025-08-08 13:16:20 -0700

    Added toggle button and task button

--- ToDoList/ContentView.swift
+++ ToDoList/ContentView.swift
@@ -8,16 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showNewTask = false
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+            HStack {
+                Text("To-do List")
+                    .font(.system(size: 40))
+                         .fontWeight(.black)
+                         Spacer()
+                Button {
+
+                } label: {
+                    Text("+")
+                        .font(.title)
+                        .fontWeight(.bold)
+                }
+            }//HStack end
+            .padding()
+        }//VStack end
+    }// body end
+}//stuct end
 
 #Preview {
     ContentView()
